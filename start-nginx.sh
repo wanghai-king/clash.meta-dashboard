91b170a237d87175fe80973b1c7ec9bc7a5d5a81
wanghai-king <77134863+wanghai-king@users.noreply.github.com>
2024-02-27 10:52:53 +0800

    Update start-nginx.sh

--- start-nginx.sh
+++ start-nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i "s|http://127.0.0.1:9090|$YACD_DEFAULT_BACKEND|" /usr/share/nginx/html/index.html
-exec nginx -g "daemon off;"
+echo "Starting Nginx..."
+nginx -g 'daemon off;' || echo "Failed to start Nginx"
